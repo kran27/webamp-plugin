19bad6c1e8027c4fa622933be4048c764a56eef3
Kran <56488228+kran27@users.noreply.github.com>
2024-03-03 01:18:32 -0800

    add plugin files

---
+++ dist/plugin.js
@@ -0,0 +1,6 @@
+exports.version = 1
+exports.description = "Use Webamp from within HFS"
+exports.apiRequired = 8.1
+exports.repo = "kran27/webamp-plugin"
+exports.frontend_js = ['main.js', 'https://unpkg.com/webamp@latest/built/webamp.bundle.min.js'] //, 'https://unpkg.com/butterchurn@latest/lib/butterchurn.min.js'] // butterchurn isn't easily usable yet
+exports.frontend_css = 'webamp.css'
---
+++ dist/public/main.js
@@ -0,0 +1,47 @@
+{
+    HFS.onEvent('beforeHeader', () => `<div id='player'/>`)
+
+    let webamp = null;
+    let closed = false;
+
+    function initWebamp() {
+        webamp = new Webamp({
+            zIndex: 10,
+            handleTrackDropEvent: async (event) => {
+                // allow dropping files from HFS
+                //get url from drop:
+                const url = event.dataTransfer.getData('text/plain');
+                webamp.appendTracks([{ url: url }]);
+            }
+        });
+        webamp.onClose(() => { closed = true; });
+    }
+
+    HFS.onEvent('fileMenu', ({ entry }) =>
+        /mp3|wav|aac|ogg|flac/.test(entry.ext) &&
+        { label: "Load Into Webamp", icon: 'audio', onClick: () => play(entry) })
+
+    function play(entry) {
+        if (!webamp) {
+            initWebamp();
+            webamp.renderWhenReady(document.getElementById('player'));
+        } else {
+            webamp.appendTracks([{ url: entry.uri }]);
+            if (closed) {
+                webamp.reopen();
+            }
+        }
+    }
+
+    HFS.onEvent('fileMenu', ({ entry }) =>
+        entry.ext == 'wsz' &&
+        { label: "Load Webamp Skin", icon: 'doc', onClick: () => loadSkin(entry.uri) })
+
+    function loadSkin(url) {
+        if (!webamp) {
+            initWebamp();
+        }
+        webamp.setSkinFromUrl(url);
+        webamp.renderWhenReady(document.getElementById('player'));
+    }
+}
